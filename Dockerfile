1a21bbc9f74cc82e70410f8e9988d25c024cff00
Nate Dean <nate.dean@selectquote.com>
2018-01-15 13:11:46 -0600

    Deletes a bunch of unused stuff

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python
-LABEL name "my-docker-deployment"
+FROM python:3
+LABEL name "reporting"
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 EXPOSE 5000
-RUN python manage.py runserver
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, render_template
+from get_report_data import get_report_data
+
+app = Flask(__name__, static_folder="client/build/static", template_folder="client/build")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/report")
+def report():
+    report_data = get_report_data()
+    return jsonify(report_data)
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+
+    
